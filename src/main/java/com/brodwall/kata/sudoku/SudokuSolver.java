0c63415ce916fcdb1928db4b329606948fc01382
Johannes Brodwall <johannes@brodwall.com>
2010-04-05 01:04:07 +0200

    shouldFindSolutionWhenCellHasOneOption runs

--- src/main/java/com/brodwall/kata/sudoku/SudokuSolver.java
+++ src/main/java/com/brodwall/kata/sudoku/SudokuSolver.java
@@ -3,10 +3,12 @@ package com.brodwall.kata.sudoku;
 public class SudokuSolver {
     private static final int SIZE = 9;
 
-    public boolean findSolution(SudokuBoard solver) {
+    public boolean findSolution(SudokuBoard board) {
         for (int index=0; index<SIZE*SIZE; index++) {
             int row = index/SIZE, column = index%SIZE;
-            if (!solver.isFilled(row, column)) return false;
+            if (!board.isFilled(row, column)) {
+                if (board.getSolutionsFor(row, column).isEmpty()) return false;
+            }
         }
         return true;
     }
