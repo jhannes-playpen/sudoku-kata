f01da883ea48675872a218b72ced8ddafce5707f
Johannes Brodwall <johannes@brodwall.com>
2010-04-05 00:58:48 +0200

    shouldNotFindSolutionWhenCellHasNoOptions fails

--- src/main/java/com/brodwall/kata/sudoku/SudokuBoard.java
+++ src/main/java/com/brodwall/kata/sudoku/SudokuBoard.java
@@ -1,7 +1,13 @@
 package com.brodwall.kata.sudoku;
 
+import java.util.List;
+
 public class SudokuBoard {
     public boolean isFilled(int row, int column) {
         return false;
+    }
+
+    public List<Integer> getSolutionsFor(int row, int column) {
+        return null;
     }
 }
--- src/test/java/com/brodwall/kata/sudoku/SudokuSolverTest.java
+++ src/test/java/com/brodwall/kata/sudoku/SudokuSolverTest.java
@@ -2,17 +2,33 @@ package com.brodwall.kata.sudoku;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.fest.assertions.Assertions.assertThat;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class SudokuSolverTest {
+    private SudokuBoard board = mock(SudokuBoard.class);
+    private SudokuSolver solver = new SudokuSolver();
+
     @Test
     public void shouldFindSolutionToFilledBoard() {
-        SudokuBoard board = mock(SudokuBoard.class);
         when(board.isFilled(anyInt(), anyInt())).thenReturn(true);
-        SudokuSolver solver = new SudokuSolver();
         assertThat(solver.findSolution(board)).isTrue();
+    }
+
+    @Test
+    public void shouldNotFindSolutionWhenCellHasNoOptions() throws Exception {
+        when(board.isFilled(anyInt(), anyInt())).thenReturn(true);
+        when(board.isFilled(8, 8)).thenReturn(false);
+        when(board.getSolutionsFor(8,8)).thenReturn(noOptions());
+        assertThat(solver.findSolution(board)).isFalse();
+    }
+
+    private List<Integer> noOptions() {
+        return new ArrayList<Integer>();
     }
 }
