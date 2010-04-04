07bad5ac71734361e559c52b220440dfd7a29358
Johannes Brodwall <johannes@brodwall.com>
2010-04-05 01:01:16 +0200

    shouldNotFindSolutionWhenCellHasNoOptions runs

--- src/main/java/com/brodwall/kata/sudoku/SudokuSolver.java
+++ src/main/java/com/brodwall/kata/sudoku/SudokuSolver.java
@@ -1,7 +1,13 @@
 package com.brodwall.kata.sudoku;
 
 public class SudokuSolver {
+    private static final int SIZE = 9;
+
     public boolean findSolution(SudokuBoard solver) {
+        for (int index=0; index<SIZE*SIZE; index++) {
+            int row = index/SIZE, column = index%SIZE;
+            if (!solver.isFilled(row, column)) return false;
+        }
         return true;
     }
 }
